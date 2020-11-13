badc3a6867611ad963307ef5642b2bea3703f7b3
scorpion9979 <ahmed_ihsan_tawfeek@yahoo.com>
2020-11-13 16:23:57 +0300

    Added more checks for test + refactored

--- test/KingSwapPair.lockIn.test.js
+++ test/KingSwapPair.lockIn.test.js
@@ -4,30 +4,33 @@ const MockERC20 = artifacts.require('MockERC20');
 const KingSwapPair = artifacts.require('KingSwapPair');
 const KingSwapFactory = artifacts.require('KingSwapFactory');
 
-contract('KingSwapPair::lockIn', ([alice, bob]) => {
+contract('KingSwapPair::lockIn', ([alice]) => {
     beforeEach(async () => {
-        this.factory = await KingSwapFactory.new(alice, { from: alice });
-        this.king = await KingToken.new({ from: alice });
-        await this.king.mint(alice, '100000000', { from: alice });
-        this.uni = await MockERC20.new('UNI', 'UNI', '100000000', { from: alice });
+        this.factory = await KingSwapFactory.new(alice);
+        this.king = await KingToken.new();
+        await this.king.mint(alice, '100000000');
+        this.uni = await MockERC20.new('UNI', 'UNI', '100000000');
         this.pair = await KingSwapPair.at((await this.factory.createPair(this.king.address, this.uni.address)).logs[0].args.pair);
         // add liquidity
-        await this.king.transfer(this.pair.address, '10000000', { from: alice });
-        await this.uni.transfer(this.pair.address, '10000000', { from: alice });
+        await this.king.transfer(this.pair.address, '10000000');
+        await this.uni.transfer(this.pair.address, '10000000');
         await this.pair.mint(alice);
-        await this.king.transfer(this.pair.address, '10000000', { from: alice });
-        await this.uni.transfer(this.pair.address, '10000000', { from: alice });
+        await this.king.transfer(this.pair.address, '10000000');
+        await this.uni.transfer(this.pair.address, '10000000');
     });
 
     context('lockedIn0 == true && lockedIn1 == false', () => {
         beforeEach(async () => {
-            const token0 = await this.pair.token0.call();
-            const token1 = await this.pair.token1.call();
-            await this.factory.lockInPair(token0, token1, true, false);
+            this.token0 = await this.pair.token0.call();
+            this.token1 = await this.pair.token1.call();
+            await this.factory.lockInPair(this.token0, this.token1, true, false);
         });
 
         it('Should allow swap on: (amount0Out == 0 && amount1Out != 0)', async () => {
+            let balanceBefore = await (await MockERC20.at(this.token1)).balanceOf(alice);
             await this.pair.swap('0', '10', alice, []);
+            let balanceAfter = await (await MockERC20.at(this.token1)).balanceOf(alice);
+            assert.equal(balanceAfter.sub(balanceBefore), '10');
         });
     
         xit('Should forbid swap on: (amount0Out == 0 && amount1Out == 0)', async () => {
@@ -45,9 +48,9 @@ contract('KingSwapPair::lockIn', ([alice, bob]) => {
 
     context('lockedIn0 == true && lockedIn1 == true', () => {
         beforeEach(async () => {
-            const token0 = await this.pair.token0.call();
-            const token1 = await this.pair.token1.call();
-            await this.factory.lockInPair(token0, token1, true, true);
+            this.token0 = await this.pair.token0.call();
+            this.token1 = await this.pair.token1.call();
+            await this.factory.lockInPair(this.token0, this.token1, true, true);
         });
 
         it('Should forbid swap on: (amount0Out == 0 && amount1Out != 0)', async () => {
@@ -69,13 +72,16 @@ contract('KingSwapPair::lockIn', ([alice, bob]) => {
 
     context('lockedIn0 == false && lockedIn1 == false', () => {
         beforeEach(async () => {
-            const token0 = await this.pair.token0.call();
-            const token1 = await this.pair.token1.call();
-            await this.factory.lockInPair(token0, token1, false, false);
+            this.token0 = await this.pair.token0.call();
+            this.token1 = await this.pair.token1.call();
+            await this.factory.lockInPair(this.token0, this.token1, false, false);
         });
 
         it('Should allow swap on: (amount0Out == 0 && amount1Out != 0)', async () => {
+            let balanceBefore = await (await MockERC20.at(this.token1)).balanceOf(alice);
             await this.pair.swap('0', '10', alice, []);
+            let balanceAfter = await (await MockERC20.at(this.token1)).balanceOf(alice);
+            assert.equal(balanceAfter.sub(balanceBefore), '10');
         });
     
         xit('Should forbid swap on: (amount0Out == 0 && amount1Out == 0)', async () => {
@@ -83,19 +89,28 @@ contract('KingSwapPair::lockIn', ([alice, bob]) => {
         });
     
         it('Should allow swap on: (amount0Out != 0 && amount1Out != 0)', async () => {
+            let balance0Before = await (await MockERC20.at(this.token0)).balanceOf(alice);
+            let balance1Before = await (await MockERC20.at(this.token1)).balanceOf(alice);
             await this.pair.swap('10', '10', alice, []);
+            let balance0After = await (await MockERC20.at(this.token0)).balanceOf(alice);
+            let balance1After = await (await MockERC20.at(this.token1)).balanceOf(alice);
+            assert.equal(balance0After.sub(balance0Before), '10');
+            assert.equal(balance1After.sub(balance1Before), '10');
         });
     
         it('Should allow swap on: (amount0Out != 0 && amount1Out == 0)', async () => {
+            let balanceBefore = await (await MockERC20.at(this.token0)).balanceOf(alice);
             await this.pair.swap('10', '0', alice, []);
+            let balanceAfter = await (await MockERC20.at(this.token0)).balanceOf(alice);
+            assert.equal(balanceAfter.sub(balanceBefore), '10');
         });
     });
 
     context('lockedIn0 == false && lockedIn1 == true', () => {
         beforeEach(async () => {
-            const token0 = await this.pair.token0.call();
-            const token1 = await this.pair.token1.call();
-            await this.factory.lockInPair(token0, token1, false, true);
+            this.token0 = await this.pair.token0.call();
+            this.token1 = await this.pair.token1.call();
+            await this.factory.lockInPair(this.token0, this.token1, false, true);
         });
 
         it('Should forbid swap on: (amount0Out == 0 && amount1Out != 0)', async () => {
@@ -111,7 +126,10 @@ contract('KingSwapPair::lockIn', ([alice, bob]) => {
         });
     
         it('Should allow swap on: (amount0Out != 0 && amount1Out == 0)', async () => {
+            let balanceBefore = await (await MockERC20.at(this.token0)).balanceOf(alice);
             await this.pair.swap('10', '0', alice, []);
+            let balanceAfter = await (await MockERC20.at(this.token0)).balanceOf(alice);
+            assert.equal(balanceAfter.sub(balanceBefore), '10');
         });
     });
 })
